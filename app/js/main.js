eed657a9cf41bb1a33c51eaa9544946c6fa201f3
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2022-12-15 07:47:01 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 $(function () {
-  
+
+  $(".content__body span").on("click", function (event) {
+    event.preventDefault();
+
+		const id  = $(this).attr('data-href'),
+			top = $(id).offset().top - 100
+    $('body,html').animate({scrollTop: top}, 1500);
+    
+    
+  });
+
 });
 
